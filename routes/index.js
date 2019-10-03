34c4a181aae5eabe069af8d50450e6dbabcf7596
EJ Ozyazgan <ejozyazgan@gmail.com>
2019-10-02 18:37:57 -0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -8,40 +8,40 @@ var router = express.Router();
 //  If you have two of a kind raises for twice the minimum amount
 //  Otherwise calls
 router.post('/bet', function (req, res, next) {
-  gs = game_state['state']
-  p = gs['players']
-  me = p[gs['me']]
+  let gs = game_state['state']
+  let p = gs['players']
+  let me = p[gs['me']]
 
-  rank = 0
+  let rank = 0
 
-  if (me['cards'][0]['rank'] == '8')
+  if (me['cards'][0]['rank'] === '8')
     rank += 8;
-  else if (me['cards'][0]['rank'] == '9')
+  else if (me['cards'][0]['rank'] === '9')
     rank += 9;
-  else if (me['cards'][0]['rank'] == '10')
+  else if (me['cards'][0]['rank'] === '10')
     rank += 10;
-  else if (me['cards'][0]['rank'] == 'J')
+  else if (me['cards'][0]['rank'] === 'J')
     rank += 11;
-  else if (me['cards'][0]['rank'] == 'Q')
+  else if (me['cards'][0]['rank'] === 'Q')
     rank += 12;
-  else if (me['cards'][0]['rank'] == 'K')
+  else if (me['cards'][0]['rank'] === 'K')
     rank += 13;
   else if (me['cards'][0]['rank'] == 'A')
     rank += 14;
 
-  if (me['cards'][1]['rank'] == '8')
+  if (me['cards'][1]['rank'] === '8')
     rank += 8;
-  else if (me['cards'][1]['rank'] == '9')
+  else if (me['cards'][1]['rank'] === '9')
     rank += 9;
-  else if (me['cards'][1]['rank'] == '10')
+  else if (me['cards'][1]['rank'] === '10')
     rank += 10;
-  else if (me['cards'][1]['rank'] == 'J')
+  else if (me['cards'][1]['rank'] === 'J')
     rank += 11;
   else if (me['cards'][1]['rank'] == 'Q')
     rank += 12;
-  else if (me['cards'][1]['rank'] == 'K')
+  else if (me['cards'][1]['rank'] === 'K')
     rank += 13;
-  else if (me['cards'][1]['rank'] == 'A')
+  else if (me['cards'][1]['rank'] === 'A')
     rank += 14;
 
   if (rank >= 24 && me['chips'] >= gs['minimumRaiseAmount'] * 2)
