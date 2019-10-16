1535e7fabf853d5d82d438f3e409cdb1dcc4802e
EJOzyazgan <ejozyazgan@gmail.com>
2019-10-16 11:44:09 -0700

    New leave setup

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ var router = express.Router();
 //  If you have two of a kind raises for twice the minimum amount
 //  Otherwise calls
 router.post('/bet', function (req, res, next) {
-  let gs = req.body['state']
-  let p = gs['players']
-  let me = p[gs['me']]
+  let gs = req.body['state'];
+  let p = gs['players'];
+  let me = p[gs['me']];
 
-  let rank = 0
+  let rank = 0;
 
   if (me['cards'][0]['rank'] === '8')
     rank += 8;
@@ -44,13 +44,22 @@ router.post('/bet', function (req, res, next) {
   else if (me['cards'][1]['rank'] === 'A')
     rank += 14;
 
+  let myBet = 0
+  let shouldLeave = false;
+
   if (rank >= 24 && me['chips'] >= gs['minimumRaiseAmount'] * 2)
-    return res.status(200).send('' + gs['minimumRaiseAmount'] * 1.5);
+    myBet = gs['minimumRaiseAmount'] * 2;
   else if (rank >= 20 && me['chips'] >= gs['minimumRaiseAmount'] * 1.5)
-    return res.status(200).send('' + gs['minimumRaiseAmount'] * 1.5);
-  else if (rank >= 24 && me['chips'] >= gs['minimumRaiseAmount'])
-    return res.status(200).send('' + gs['minimumRaiseAmount']);
-  return res.status(200).send('' + gs['callAmount']);
+    myBet = gs['minimumRaiseAmount'] * 1.5;
+  else if (rank >= 16 && me['chips'] >= gs['minimumRaiseAmount'])
+    myBet = gs['minimumRaiseAmount'];
+
+  if (me['chips'] >= me['buyIn'] * 1.5) 
+    shouldLeave = true;
+  else if (me['chips'] <= me['buyIn'] * 0.5)
+    shouldLeave = true;
+
+  return res.status(200).json({bet: myBet, leave: shouldLeave});
 });
 
 module.exports = router;
